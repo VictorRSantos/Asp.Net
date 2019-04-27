408ce9c10ba6b7aa5561a5cc02b3fd055ba1bca5
Victor Rodrigues dos Santos <victor.rsantos@outlook.com>
2019-04-27 08:53:27 -0300

    Testando Commit

---
+++ BibliotecaImpacta/Models/Livro.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BibliotecaImpacta.Models
+{
+    public class Livro
+    {
+
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+
+    }
+}
